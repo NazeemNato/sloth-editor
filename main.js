91722b454a045a738802ad39bd2a03a6b8bfd456
nazeem <nazeemnob18@gmail.com>
2022-02-20 20:16:38 +0530

    monaco editor instead of codemirror

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
-import { basicSetup, EditorState, EditorView } from '@codemirror/basic-setup';
-import { tags, HighlightStyle } from "@codemirror/highlight"
+// import { basicSetup, EditorState, EditorView } from '@codemirror/basic-setup';
+// import { tags, HighlightStyle } from "@codemirror/highlight"
+import * as monaco from 'monaco-editor';
 
-const myHighlightStyle = HighlightStyle.define([
-  { tag: tags.keyword, color: "#fc6" },
-  { tag: tags.comment, color: "#f5d", fontStyle: "italic" }
-])
 // setup our intepeter wasm module
 const go = new Go();
 WebAssembly.instantiateStreaming(fetch("sloth.wasm"), go.importObject).then(
@@ -38,29 +35,81 @@ if(!isGreater(mul)) {
 print(myArray[0])
 `
 
-const initialState = EditorState.create({
-  doc: initPgm,
-  extensions: [
-    basicSetup,
-    // javascript(),
-    // keymap.of([uppercaseKeybinding]),
-    // myTheme,
-    myHighlightStyle,
-  ],
+
+monaco.languages.register({ id: 'sloth' });
+
+monaco.languages.setMonarchTokensProvider('sloth', {
+  tokenizer: {
+    root: [
+      [/\b(?:print|if|else|fun|var|concat|true|false|concat|len)\b/, 'keyword'],
+      [/\b(?:[0-9]+)\b/, 'number'],
+      [/\b(?:[a-zA-Z_][a-zA-Z0-9_]*)\b/, 'identifier'],
+      [/[,;]/, 'delimiter'],
+      [/[()]/, 'paren'],
+      [/[\s]+/, 'whitespace'],
+      [/"(?:[^\\"]|\\.)*"/, 'string'],
+      [/\[(?:[^\]]|\\.)*\]/, 'string'],
+    ],
+  },
+});
+// completion
+monaco.languages.registerCompletionItemProvider('sloth', {
+  provideCompletionItems: () => {
+    var suggestions = [
+      {
+        label: 'concat',
+        kind: monaco.languages.CompletionItemKind.Keyword,
+        insertText: 'concat(${1:word1},${2:word2})',
+        insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+        documentation: 'concatenate strings',
+      },
+      {
+        label: 'ifelse',
+        kind: monaco.languages.CompletionItemKind.Snippet,
+        insertText: ['if (${1:condition}) {', '\t$0', '} else {', '\t', '}'].join('\n'),
+        insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+        documentation: 'If-Else Statement'
+      },
+      {
+        label: 'len',
+        kind: monaco.languages.CompletionItemKind.Keyword,
+        insertText: 'len(${1:word1})',
+        insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+        documentation: 'length of string or array',
+      },
+      {
+        label: 'fun',
+        kind: monaco.languages.CompletionItemKind.Keyword,
+        insertText: ['var myFunc = fun(${1:param}) {', '\treturn $1', '}'].join('\n'),
+        insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+        documentation: 'create sloth function',
+      },
+      {
+        label: 'print',
+        kind: monaco.languages.CompletionItemKind.Keyword,
+        insertText: 'print(${1:word})',
+        insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+        documentation: 'print function in sloth',
+      }
+    ];
+    return { suggestions: suggestions };
+  }
 });
 
-const view = new EditorView({
-  parent: document.getElementById('playground'),
-  state: initialState,
+
+monaco.editor.create(document.getElementById('playground'), {
+  value: initPgm,
+  automaticLayout: true,
+  fontSize: 16,
+  language: 'sloth',
 });
-
-window.view = view;
-
 
 // add button listener to run-sloth
 document.getElementById('run-sloth').addEventListener('click', () => {
   document.getElementById('console-output').innerHTML = '';
-  const code = view.state.doc.text.join("\n")
+  let code = monaco.editor.getModels()[0].getValue();
+  console.log(code);
+  // const code = view.state.doc.text.join("\n")
   const response = mySloth(code)
   // set response to console-output
   if (response.trim().length !== 0) {
